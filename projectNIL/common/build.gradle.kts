98dd5e686bcce2fbb8710f25710ddaa81205d78c
Raj <rajp152k@gmail.com>
2025-12-22 10:15:17 +0530

    fix: gradle version catalog resolution

--- projectNIL/common/build.gradle.kts
+++ projectNIL/common/build.gradle.kts
@@ -1,6 +1,11 @@
 group = "com.projectnil"
 version = "0.0.1-SNAPSHOT"
 
+plugins {
+        id("java")
+        alias(libs.plugins.spring.dependency.management)
+}
+
 dependencies {
-             implementation(libs.spring.boot.starter.data.jpa)
+        implementation(libs.spring.boot.starter.data.jpa)
 }
