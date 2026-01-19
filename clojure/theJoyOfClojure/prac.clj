91339396eca43f1a79efdd3a722503d6f21bdfa5
Raj <rajp152k@gmail.com>
2026-01-19 18:29:59 +0530

    clj: misc refactors

--- clojure/theJoyOfClojure/prac.clj
+++ clojure/theJoyOfClojure/prac.clj
@@ -1,19 +1,2 @@
-(ns prac)
+(ns )
 
-(defn _+
-  ([] 0)
-  ([x] x)
-  ([x y] (. clojure.lang.Numbers (add x y)))
-  ([x y & more]
-   (reduce + (+ x y) more)))
-
-(_+ 1 2 3)
-
-(def x 1)
-(def y 2)
-
-(let [r 5
-      pi 3.1415
-      r-squared (* r r)]
-  (println "for radius" r)
-  (println "area is " (* pi r-squared)))
