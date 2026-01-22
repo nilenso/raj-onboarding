e078f47180141b5127f6bdf644d607dfb3099140
Raj <rajp152k@gmail.com>
2026-01-22 19:08:24 +0530

    dev setup updates

---
+++ pNIL/pnil/development/src/pracapi.clj
@@ -0,0 +1,25 @@
+(ns pracapi
+  (:require [org.httpkit.server :as hk-server]
+            [nrepl.server :as nrepl]
+            [reitit.ring :as ring]))
+
+(defn- root-handler [_req]
+  {:status  200
+   :body "Sentinel Body"
+   :headers {"Content-Type" "text/html"}})
+
+(defn- status-handler [_req]
+  {:status 200
+   :body "OK"
+   :headers {"Content-Type" "text/html"}})
+
+(def app
+  (ring/ring-handler
+   (ring/router [["/" {:get root-handler}]
+                 ["/status" {:get status-handler}]])))
+
+(defn -main []
+  (println  "starting httpkit-server")
+  (hk-server/run-server app {:port 8080})
+  (println  "starting nrepl-server")
+  (nrepl/start-server :port 8081))
