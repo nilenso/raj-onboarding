347fb930918e4d3c2ea65aece5112414fc7d08ff
Raj <rajp152k@gmail.com>
2026-01-27 19:09:44 +0530

    c4 progress

--- prac/theJoyOfClojure/src/c4.clj
+++ prac/theJoyOfClojure/src/c4.clj
@@ -3,3 +3,31 @@
             [clojure.java.javadoc :refer [javadoc]]))
 
 
+(def testmap {:a 'a :b 'b})
+(:a testmap)
+
+(doc iterate)
+
+(doc range)
+
+(doc take)
+ 
+(doc transduce)
+
+(defn pour [lb ub]
+  (cond
+    (= ub :toujours) (iterate inc lb)
+    :else (range lb ub)))
+
+(pour 0 10)
+
+(take 10 (pour 0 :toujours))
+
+::test-q-key
+
+(defn best [f xs]
+  (reduce #(if (f %1 %2) %1 %2) xs))
+
+(best > (range 10))
+(best < (range 10))
+
