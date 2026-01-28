1f5cc5d7e700221e3a4fcc92d2d81ee97e794e7c
Raj <rajp152k@gmail.com>
2026-01-28 15:58:26 +0530

    c5 sync

--- prac/theJoyOfClojure/src/c5.clj
+++ prac/theJoyOfClojure/src/c5.clj
@@ -2,3 +2,21 @@
   (:require [clojure.repl :refer [doc]]
             [clojure.java.javadoc :refer [javadoc]]))
 (def j javadoc)
+
+(doc into-array)
+
+(def ds (into-array [1 2 3]))
+
+ds
+
+(seq ds)
+
+(j (class (seq ds)))
+
+(doc aset)
+
+(aset ds 1 0)
+
+(seq ds)
+
+(doc type)
