837c488a1e91c3cd3a5dfdf6753ac380821e2d8e
Raj <rajp152k@gmail.com>
2026-02-02 16:05:03 +0530

    test: init api/db_test

--- pNIL/test/api/db_test.clj
+++ pNIL/test/api/db_test.clj
@@ -1,3 +1,19 @@
 (ns api.db-test
   (:require
-   [clojure.test :refer :all]))
+   [next.jdbc :as jdbc ]
+   [next.jdbc.sql :as sql]
+   [next.jdbc.types :refer [as-other]]
+   [clojure.test :as t :refer [testing is deftest]]))
+
+(def test-db-spec {:dbtype "postgres"
+                   :dbname "projectnil"
+                   :user "projectnil"
+                   :password "projectnil"
+                   :host "localhost"
+                   :port 5433}) ; 5433 when testing
+
+
+(def datasource (jdbc/get-datasource test-db-spec))
+
+(deftest add-function
+  )
