2f933fb35b40b603fb4e3b3a7ac55a982adbc888
Raj <rajp152k@gmail.com>
2026-02-04 21:47:16 +0530

    pNIL:utils:cider-nrepl-handler added

--- pNIL/src/utils.clj
+++ pNIL/src/utils.clj
@@ -2,18 +2,23 @@
   (:require
    [aero.core :refer [read-config]]
    [clojure.java.io :as io]
-   [nrepl.server :as nrepl]))
+   [nrepl.server :as nrepl]
+   [cider.nrepl :refer [cider-nrepl-handler]]))
 
 (defn env-predicated-nrepl-init [configs component]
   "Runs the nrepl server conditioned on the current runtime environment"
-  (when (= (:env configs)
-           "DEV")
-    (let [nrepl-port (-> configs
-                         (component)
-                         (:nrepl-port))]
-      (println "starting" component "nrepl | port" nrepl-port)
-      (nrepl/start-server :port nrepl-port)
-      (println component "nrepl started"))))
+  (if (= (:env configs)
+         "DEV")
+    (do
+      (println "DEV environment detected")
+      (let [nrepl-port (-> configs
+                           (component)
+                           (:nrepl-port))]
+        (println "starting" component "nrepl | port" nrepl-port)
+        (nrepl/start-server :port nrepl-port
+                            :handler cider-nrepl-handler)
+        (println component "nrepl started")))
+    (println "Non-DEV environment" (:env configs) "detected - skipping" component "nrepl startup")))
 
 (defn read-config-file [path]
   "Reads the config file at the given path and returns it as a map"
