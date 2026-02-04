5639beeaae430c999c4e5581c367a910d4e22042
Raj <rajp152k@gmail.com>
2026-02-04 13:57:48 +0530

    pNIL: api db test init

--- pNIL/test/api/db_test.clj
+++ pNIL/test/api/db_test.clj
@@ -1,10 +1,12 @@
 (ns api.db-test
   (:require
+   [api.db :refer [add-function]]
    [next.jdbc :as jdbc ]
    [next.jdbc.sql :as sql]
    [next.jdbc.types :refer [as-other]]
    [clojure.test :as t :refer [testing is deftest]]))
 
+;; make fixture
 (def test-db-spec {:dbtype "postgres"
                    :dbname "projectnil"
                    :user "projectnil"
@@ -13,7 +15,19 @@
                    :port 5433}) ; 5433 when testing
 
 
+;; make fixture
 (def datasource (jdbc/get-datasource test-db-spec))
 
-(deftest add-function
-  )
+;; pre-exec fixture
+
+(deftest add-function-test
+  (testing "adding a function to the database"
+    (add-function datasource
+                  {:name "echo"
+                   :description "echos input, unaltered"
+                   :language "assemblyscript"
+                   :source "export function handle(input: string): string {return input;}"
+                   :status (as-other "PENDING")})
+    (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
+      (is (= 1 (count functions)))
+      (is (= "echo" (:name (first functions)))))))
