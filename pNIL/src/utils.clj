9dad7ed43e7fd5d429f56f5b4ed128263c379378
Raj <rajp152k@gmail.com>
2026-02-04 20:53:32 +0530

    pNIL:utils:predicated nrepl init helper + config loader

--- pNIL/src/utils.clj
+++ pNIL/src/utils.clj
@@ -1,2 +1,21 @@
-(ns utils)
+(ns utils
+  (:require
+   [aero.core :refer [read-config]]
+   [clojure.java.io :as io]
+   [nrepl.server :as nrepl]))
+
+(defn env-predicated-nrepl-init [configs component]
+  "Runs the nrepl server conditioned on the current runtime environment"
+  (when (= (:env configs)
+           "DEV")
+    (let [nrepl-port (-> configs
+                         (component)
+                         (:nrepl-port))]
+      (println "starting" component "nrepl | port" nrepl-port)
+      (nrepl/start-server :port nrepl-port)
+      (println component "nrepl started"))))
+
+(defn read-config-file [path]
+  "Reads the config file at the given path and returns it as a map"
+  (read-config (io/resource path)))
 
