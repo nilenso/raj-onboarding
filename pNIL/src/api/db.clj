95bd00783ceddc1f19d46a50dbd29f300521bc2d
Raj <rajp152k@gmail.com>
2026-02-05 18:47:04 +0530

    pnil/api/db: functions db api fin

--- pNIL/src/api/db.clj
+++ pNIL/src/api/db.clj
@@ -3,7 +3,8 @@
    [next.jdbc :as jdbc :refer [execute! get-datasource]]
    [next.jdbc.sql :as sql :refer [insert!]]
    [next.jdbc.types :as types]
-   [utils :as u]))
+   [taoensso.telemere :as t :refer [log! error!]]
+   [utils :as u :refer [prog1]]))
 
 (defn get-profile-datasource
   "create a next.jdbc datasource for the given profile (:dev or :test)"
@@ -19,30 +20,43 @@
 (defn truncate-all-tables
   "truncate all tables in the database"
   [datasource]
-  (execute! datasource ["TRUNCATE FUNCTIONS, EXECUTIONS;"]))
+  (try
+    (prog1
+     (execute! datasource ["TRUNCATE FUNCTIONS, EXECUTIONS;"])
+     (log! :debug "All tables truncated successfully"))
+    (catch Exception e
+      (error! ::truncate-failed e)
+      (throw e))))
 
 (defn get-functions
   "retrieve all functions from the FUNCTIONS table"
   [datasource]
-  (execute! datasource ["SELECT * FROM FUNCTIONS;"]))
-
+  (try
+    (prog1 
+     (execute! datasource ["SELECT * FROM FUNCTIONS;"])
+     (log! :debug "Functions retrieved successfully"))
+    (catch Exception e
+      (error! ::function-retrieval-failed e)
+      (throw e))))
 
 (defn add-function
   "insert a new function into the FUNCTIONS table"
   [datasource fn-map]
-  (insert! datasource :functions fn-map))
+  (try 
+    (prog1
+     (insert! datasource :functions fn-map)
+     (log! :debug ::function-addition-successful fn-map))
+    (catch Exception e
+      (error! ::function-addition-failed e {:function-name (:name fn-map)})
+      (throw e))))
 
-(comment
-  (def datasource (get-profile-datasource :dev))               ; :test or :dev
-
-  (truncate-all-tables datasource)
-
-  (add-function datasource {:name "name"
-                            :description "desc"
-                            :language "asc"
-                            :source "source"
-                            :status (types/as-other "PENDING")})
-
-  (get-functions datasource)
-
-  (truncate-all-tables datasource))
+(defn delete-function
+  "delete a function from the FUNCTIONS table by id"
+  [datasource fn-id]
+  (try
+    (prog1 
+     (execute! datasource ["DELETE FROM FUNCTIONS WHERE id = ?;" fn-id])
+     (log! :debug ::function-deletion-successful {:function-id fn-id}))
+    (catch Exception e
+      (error! ::function-deletion-failed e {:function-id fn-id})
+      (throw e))))
