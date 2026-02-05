ad8d7cc38c70cccb54686add1363737300fb3da7
Raj <rajp152k@gmail.com>
2026-02-06 01:26:47 +0530

    pnil/test/api: update tests (datasource -> conn pooling)

--- pNIL/test/api/db_test.clj
+++ pNIL/test/api/db_test.clj
@@ -2,59 +2,54 @@
   (:require
    [api.db :as api-db]
    [next.jdbc :as jdbc]
-   [next.jdbc.types :refer [as-other]]
+   [next.jdbc.types :as jdbc-types]
    [clojure.test :as t :refer [testing is deftest use-fixtures]]))
 
-(def ^:dynamic *datasource* nil)
+(use-fixtures :once 
+  (fn [f]
+    (api-db/start-pool! :test)
+    (try (f)
+         (finally (api-db/stop-pool!)))))
 
-(use-fixtures :each (fn [test-fn]
-                      (let [datasource (api-db/get-profile-datasource :test)]
-                        (api-db/truncate-all-tables datasource)
-                        (binding [*datasource* datasource]
-                          (test-fn)))))
+(use-fixtures :each (fn [f]
+                      (api-db/truncate-all-tables)
+                      (f)))
 
 (deftest add-function-test
   (testing "adding a function to the database"
-    (let [datasource *datasource*]
-      (api-db/add-function datasource
-                           {:name "echo"
-                            :description "echos input, unaltered"
-                            :language "assemblyscript"
-                            :source "export function handle(input: string): string {return input;}"
-                            :status (as-other "PENDING")})
-      (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
-        (is (= 1 (count functions)))
-        (is (= "echo" (:functions/name (first functions))))))))
+    (api-db/add-function {:name "echo"
+                          :description "echos input, unaltered"
+                          :language "assemblyscript"
+                          :source "export function handle(input: string): string {return input;}"
+                          :status (jdbc-types/as-other "PENDING")})
+    (let [functions (jdbc/execute! (api-db/get-pool) ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
+      (is (= 1 (count functions)))
+      (is (= "echo" (:functions/name (first functions)))))))
 
 (deftest get-functions-test
   (testing "retrieving functions from the database"
-    (let [datasource *datasource*]
-      (api-db/add-function datasource
-                           {:name "func1"
-                            :description "first function"
-                            :language "python"
-                            :source "def handle(input): return input"
-                            :status (as-other "PENDING")})
-      (api-db/add-function datasource
-                           {:name "func2"
-                            :description "second function"
-                            :language "javascript"
-                            :source "exports.handle = function(input) { return input; }"
-                            :status (as-other "PENDING")})
-      (let [functions (api-db/get-functions datasource)]
-        (is (= 2 (count functions)))
-        (is (= #{"func1" "func2"} (set (map :functions/name functions))))))))
+    (api-db/add-function {:name "func1"
+                          :description "first function"
+                          :language "python"
+                          :source "def handle(input): return input"
+                          :status (jdbc-types/as-other "PENDING")})
+    (api-db/add-function {:name "func2"
+                          :description "second function"
+                          :language "javascript"
+                          :source "exports.handle = function(input) { return input; }"
+                          :status (jdbc-types/as-other "PENDING")})
+    (let [functions (api-db/get-functions)]
+      (is (= 2 (count functions)))
+      (is (= #{"func1" "func2"} (set (map :functions/name functions)))))))
 
 (deftest delete-function-test
   (testing "deleting a function from the database"
-    (let [datasource *datasource*
-          fn-entry (api-db/add-function datasource
-                                        {:name "tobedeleted"
+    (let [fn-entry (api-db/add-function {:name "tobedeleted"
                                          :description "to be deleted"
                                          :language "ruby"
                                          :source "def handle(input); input; end"
-                                         :status (as-other "PENDING")})
+                                         :status (jdbc-types/as-other "PENDING")})
           fn-id (:functions/id fn-entry)]
-      (api-db/delete-function datasource fn-id)
-      (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE id = ?" fn-id])]
+      (api-db/delete-function fn-id)
+      (let [functions (jdbc/execute! (api-db/get-pool) ["SELECT * FROM FUNCTIONS WHERE id = ?" fn-id])]
         (is (= 0 (count functions)))))))
