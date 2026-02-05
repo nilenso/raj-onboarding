435fd3989ad7da4b04d437ec6d1644e95119bbfa
Raj <rajp152k@gmail.com>
2026-02-05 07:36:29 +0530

    pnil:api/db-test:fixtures setup

--- pNIL/test/api/db_test.clj
+++ pNIL/test/api/db_test.clj
@@ -4,27 +4,25 @@
    [next.jdbc :as jdbc ]
    [next.jdbc.sql :as sql]
    [next.jdbc.types :refer [as-other]]
-   [clojure.test :as t :refer [testing is deftest]]))
+   [clojure.test :as t :refer [testing is deftest use-fixtures]]))
 
-;; make fixture
+(def ^:dynamic *datasource* nil)
 
+(use-fixtures :each (fn [test-fn]
+                      (let [datasource (api-db/get-profile-datasource :test)]
+                        (api-db/truncate-all-tables datasource)
+                        (binding [*datasource* datasource]
+                          (test-fn)))))
 
-;; make fixture
-(def datasource (api-db/get-profile-datasource :test))
-
-;; pre-exec fixture
-
-(deftest add-function-test
+(deftest add-function-test 
   (testing "adding a function to the database"
-    (api-db/truncate-all-tables datasource)
-
-    (api-db/add-function datasource
-                         {:name "echo"
-                          :description "echos input, unaltered"
-                          :language "assemblyscript"
-                          :source "export function handle(input: string): string {return input;}"
-                          :status (as-other "PENDING")})
-
-    (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
-      (is (= 1 (count functions)))
-      (is (= "echo" (:functions/name (first functions)))))))
+    (let [datasource *datasource*]
+      (api-db/add-function datasource
+                           {:name "echo"
+                            :description "echos input, unaltered"
+                            :language "assemblyscript"
+                            :source "export function handle(input: string): string {return input;}"
+                            :status (as-other "PENDING")})
+      (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
+        (is (= 1 (count functions)))
+        (is (= "echo" (:functions/name (first functions))))))))
