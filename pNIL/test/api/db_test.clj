cf7bb211a4d08390339babadcd32e89ece114447
Raj <rajp152k@gmail.com>
2026-02-05 07:19:10 +0530

    pnil:db-test: fix add-function test

--- pNIL/test/api/db_test.clj
+++ pNIL/test/api/db_test.clj
@@ -1,33 +1,30 @@
 (ns api.db-test
   (:require
-   [api.db :refer [add-function]]
+   [api.db :as api-db]
    [next.jdbc :as jdbc ]
    [next.jdbc.sql :as sql]
    [next.jdbc.types :refer [as-other]]
    [clojure.test :as t :refer [testing is deftest]]))
 
 ;; make fixture
-(def test-db-spec {:dbtype "postgres"
-                   :dbname "projectnil"
-                   :user "projectnil"
-                   :password "projectnil"
-                   :host "localhost"
-                   :port 5433}) ; 5433 when testing
 
 
 ;; make fixture
-(def datasource (jdbc/get-datasource test-db-spec))
+(def datasource (api-db/get-profile-datasource :test))
 
 ;; pre-exec fixture
 
 (deftest add-function-test
   (testing "adding a function to the database"
-    (add-function datasource
-                  {:name "echo"
-                   :description "echos input, unaltered"
-                   :language "assemblyscript"
-                   :source "export function handle(input: string): string {return input;}"
-                   :status (as-other "PENDING")})
+    (api-db/truncate-all-tables datasource)
+
+    (api-db/add-function datasource
+                         {:name "echo"
+                          :description "echos input, unaltered"
+                          :language "assemblyscript"
+                          :source "export function handle(input: string): string {return input;}"
+                          :status (as-other "PENDING")})
+
     (let [functions (jdbc/execute! datasource ["SELECT * FROM FUNCTIONS WHERE name = ?" "echo"])]
       (is (= 1 (count functions)))
-      (is (= "echo" (:name (first functions)))))))
+      (is (= "echo" (:functions/name (first functions)))))))
