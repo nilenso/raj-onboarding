47e55d46be6d7ac57192a5957dbf2c8fd526abfe
Raj <rajp152k@gmail.com>
2026-02-05 18:48:26 +0530

    pnil/utils: prog1,logging,nrepl error

--- pNIL/src/utils.clj
+++ pNIL/src/utils.clj
@@ -3,6 +3,7 @@
    [aero.core :refer [read-config]]
    [clojure.java.io :as io]
    [nrepl.server :as nrepl]
+   [taoensso.telemere :as t :refer [log! error!]]
    [cider.nrepl :refer [cider-nrepl-handler]]))
 
 (defn env-predicated-nrepl-init
@@ -10,21 +11,39 @@
   [configs component]
   (if (= (:env configs) "DEV")
     (do
-      (println "DEV environment detected")
-      (let [nrepl-port (-> configs
-                           (component)
-                           (:nrepl-port))]
-        (println "starting" component "nrepl | port" nrepl-port)
-        (nrepl/start-server :port nrepl-port
-                            :handler cider-nrepl-handler)
-        (println component "nrepl started")))
-    (println "Non-DEV environment" (:env configs) "detected - skipping" component "nrepl startup")))
+      (log! :info "DEV environment detected")
+      (try
+        (let [nrepl-port (-> configs
+                             (component)
+                             (:nrepl-port))]
+          (nrepl/start-server :port nrepl-port
+                              :handler cider-nrepl-handler)
+          (log! :info "nrepl server started port"
+                {:component component :nrepl-port nrepl-port}))
+        (catch Exception e
+          (error! ::nrepl-server-start-failed e {:component component}))))
+    (log! :info "Non-DEV environment detected - skipping nrepl startup" {:component component :env (:env configs)})))
 
 (defn- read-configs 
   "read configs, secrets from resources and merge them"
   []
-  (let [base-config (read-config (io/resource "config.edn"))
-        secrets (read-config (io/resource "secrets.edn"))]
-    (merge base-config secrets)))
+  (try
+    (let [base-config (read-config (io/resource "config.edn"))
+          secrets (read-config (io/resource "secrets.edn"))]
+      (log! :info "Configs read from resources"
+            {:base-config base-config :secrets (keys secrets)})
+      (merge base-config secrets))
+    (catch Exception e
+      (error! ::config-read-failed e)
+      (throw e))))
 
 (defonce configs (read-configs))
+
+(defmacro prog1
+  "Evaluate expr1, then evaluate and return the value of exprs...
+   (prog1 expr1 expr2 expr3 ...)
+   Returns the value of expr1."
+  [expr1 & exprs]
+  `(let [result# ~expr1]
+     ~@exprs
+     result#))
