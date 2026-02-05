6748044aea2b10566bfb0705353b30a67d200a8d
Raj <rajp152k@gmail.com>
2026-02-05 07:18:39 +0530

    pnil:utils: refactor - make config reader private

--- pNIL/src/utils.clj
+++ pNIL/src/utils.clj
@@ -7,8 +7,7 @@
 
 (defn env-predicated-nrepl-init [configs component]
   "Runs the nrepl server conditioned on the current runtime environment"
-  (if (= (:env configs)
-         "DEV")
+  (if (= (:env configs) "DEV")
     (do
       (println "DEV environment detected")
       (let [nrepl-port (-> configs
@@ -20,7 +19,7 @@
         (println component "nrepl started")))
     (println "Non-DEV environment" (:env configs) "detected - skipping" component "nrepl startup")))
 
-(defn read-configs []
+(defn- read-configs []
   "read configs, secrets from resources and merge them"
   (let [base-config (read-config (io/resource "config.edn"))
         secrets (read-config (io/resource "secrets.edn"))]
