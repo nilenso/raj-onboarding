6aa3faa7200d384001ae3ec10cacdc4cc15f9edb
Raj <rajp152k@gmail.com>
2026-02-10 13:55:46 +0530

    jour, joc: sync

--- prac/theJoyOfClojure/src/c5.clj
+++ prac/theJoyOfClojure/src/c5.clj
@@ -44,3 +44,20 @@ dsir
 
 ;; testing cider progress bar
 (Thread/sleep 5000)
+
+
+(j (class (hash-map :a 1)))
+
+(doc into)
+
+(doc transduce)
+
+(into [1 2] (map inc) [2 3])
+
+(doc comp)
+
+((comp #(take 5 %) #(map inc %) #(range %)) 10)
+
+(doc eduction)
+
+(eduction (comp (map inc) (map #(* % %))) (range 10))
