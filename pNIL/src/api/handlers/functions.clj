bc320b3e4bbe04de4faf1de5c3be4d64a5c77585
Raj <rajp152k@gmail.com>
2026-03-03 21:22:07 +0530

    get functions handler added

--- pNIL/src/api/handlers/functions.clj
+++ pNIL/src/api/handlers/functions.clj
@@ -1,2 +1,16 @@
-(ns api.handlers.functions)
+(ns api.handlers.functions
+  (:require
+   [api.db :as db]
+   [taoensso.telemere :as t :refer [log!]]))
 
+(defn get-functions-handler [req]
+  (let [functions  (map #(update-keys (dissoc % ;;unqualify keys
+                                              :wasm_binary)
+                                      (comp keyword name))
+                        (db/get-functions))]
+    (log! {:level :debug
+           :id ::get-functions-handler-called
+           :data {:num-functions (count functions)}})
+    {:status 200
+     :body functions
+     :headers {"Content-Type" "application/json"}}))
