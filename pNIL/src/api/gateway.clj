c30b6070400fbcb2be0a7bd9cb4f502d87fccd3d
Raj <rajp152k@gmail.com>
2026-03-04 08:05:35 +0530

    post functions handler done

--- pNIL/src/api/gateway.clj
+++ pNIL/src/api/gateway.clj
@@ -3,11 +3,14 @@
    [reitit.ring :as r]
    [taoensso.telemere :as t :refer [log!]]
    [org.httpkit.server :as hks]
+   [api.handlers.functions :as f]
    [api.handlers.core :as ahc]))
 
 (def app
   (r/ring-handler
-   (r/router [["/status" {:get ahc/status-handler}]])))
+   (r/router [["/status" {:get ahc/status-handler}]
+              ["/functions" {:get f/get-functions-handler
+                             :post f/post-function-handler}]])))
 
 (defn run-server
   "Starts the API server on the specified HTTP port."
@@ -17,3 +20,13 @@
            :msg "API server started successfully"
            :data {:http-port http-port}})
     stop-fn))
+
+
+
+(comment
+
+  (def stop-fn (run-server 9079))
+
+  (stop-fn)
+
+  )
--- pNIL/src/api/handlers/functions.clj
+++ pNIL/src/api/handlers/functions.clj
@@ -1,7 +1,31 @@
 (ns api.handlers.functions
   (:require
    [api.db :as db]
+   [clojure.data.json :refer [read-str write-str]]
+   [api.utils :as u :refer [throw-error!]]
+   [ring.util.response :as r]
    [taoensso.telemere :as t :refer [log!]]))
+
+(defn- sanitize-function-data [function-data]
+  (dissoc 
+   (update-keys function-data (comp keyword name))
+   :wasm_binary
+   :source
+   :compile_error))
+
+(defn post-function-handler [req]
+  (try 
+    (let [fn-data (read-str (slurp (:body req)))
+          db-ack (db/add-function fn-data)
+          fn-id (:functions/id db-ack)]
+      (log! {:level :debug
+             :id ::post-function-handler-successful
+             :data {:req-fn-body fn-data
+                    :fn-id (:functions/id db-ack)
+                    :fn-name (:functions/name db-ack)}})
+      (r/created (str "/functions/" fn-id) (write-str (sanitize-function-data db-ack))))
+    (catch Exception e
+      (throw-error! ::post-function-handler-failed e))))
 
 (defn get-functions-handler [req]
   (let [functions  (map #(update-keys (dissoc % ;;unqualify keys
