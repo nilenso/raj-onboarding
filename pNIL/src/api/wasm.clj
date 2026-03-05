816cd45ae5296c716fe5a763632696ad94851964
Raj <rajp152k@gmail.com>
2026-03-06 01:09:41 +0530

    wasm runtime java->clojure wrapper

--- pNIL/src/api/wasm.clj
+++ pNIL/src/api/wasm.clj
@@ -1,2 +1,18 @@
-(ns api.wasm)
+(ns api.wasm
+  (:import
+   [com.projectnil.api.runtime ChicoryWasmRuntime AssemblyScriptStringCodec]
+   [java.time Duration]
+   [java.nio.charset StandardCharsets]))
 
+(defn make-runtime
+  "Create a ChicoryWasmRuntime with the given timeout in milliseconds."
+  [timeout-ms]
+  (ChicoryWasmRuntime. (AssemblyScriptStringCodec.)
+                       (Duration/ofMillis timeout-ms)))
+
+(defn execute
+  "Execute a WASM binary with the given input JSON string.
+   Returns the output as a string."
+  [runtime wasm-bytes input-json]
+  (String. (.execute runtime wasm-bytes input-json)
+           StandardCharsets/UTF_8))
