68564816ec1f9ef609a11a27e24c0befa12ab22a
Raj <rajp152k@gmail.com>
2026-03-09 13:03:28 +0530

    trampoline

--- scratch/prac/theJoyOfClojure/src/c7.clj
+++ scratch/prac/theJoyOfClojure/src/c7.clj
@@ -19,3 +19,28 @@
      (only-true [this whatever] true)
      (only-false [this] false)))
  'whatever)
+
+(let  [a :a
+       b :b
+       c :c]
+  (letfn [(sa [[_ & r]]
+            (case _
+              nil true
+              :a false
+              :b (sb r)
+              :c (sc r)))
+          (sb [[_ & r]]
+            (case _
+              nil true
+              :b false
+              :a (sa r)
+              :c (sc r)))
+          (sc [[_ & r]]
+            (case _
+              nil true
+              :c false
+              :a (sa r)
+              :b (sb r)))]
+    [(trampoline sb [:a :b :c :b :a :c ])
+     (trampoline sb [:a :b :c :b :a :c :c])
+     (trampoline sa [:b :c :a :c :a :b])]))
