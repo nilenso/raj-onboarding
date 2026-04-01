07d7c4981bab497f3827782fe30a8076251bc46e
Raj <rajp152k@gmail.com>
2026-04-01 13:48:35 +0530

    joc sync

--- scratch/prac/theJoyOfClojure/src/c10.clj
+++ scratch/prac/theJoyOfClojure/src/c10.clj
@@ -1,4 +1,20 @@
-(ns c10)
+(ns c10
+  (:import java.util.concurrent.Executors))
 
 (comment
   dosync)
+
+(def thread-pool
+  (Executors/newFixedThreadPool
+   (+ 2 (.availableProcessors (Runtime/getRuntime)))))
+
+(defn dothreads!
+  [f & {thread-count :threads
+        exec-count :times
+        :or {thread-count 1 exec-count 1}}]
+  (dotimes [t thread-count]
+    (.submit thread-pool
+             #(dotimes [_ exec-count] (f)))))
+
+
+(dothreads! #(println "Hi ") :threads 2 :times 8)
